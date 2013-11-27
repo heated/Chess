6c3a022b8f39487f1b4bfddefd446a116796062e
App Academy Student <student@appacademy.io>
2013-11-26 16:19:53 -0800

    game loop set up

--- game.rb
+++ game.rb
@@ -2,7 +2,9 @@ require_relative 'board.rb'
 
 class Game
   attr_accessor :board
-  def initialize(player_1, player_2)
+  def initialize(players)
+    @white = players[:white]
+    @black = players[:black]
     @board = Board.new
     setup_pieces
   end
@@ -19,16 +21,46 @@ class Game
 
 
   def play
+    turn = 0
     until @board.over?
-
+      p @board
+      begin
+        if turn.odd?
+          @black.play_turn(@board, :b)
+        else
+          @white.play_turn(@board, :w)
+        end
+      rescue => e
+        puts e.message
+        retry
+      end
+      turn += 1
     end
-  end
-
-  def get_move
-
   end
 end
 
-funtimes = Game.new(nil, nil)
+class HumanPlayer
 
-p funtimes.board
+  def initialize(name = "John")
+    @name = name
+  end
+
+  def play_turn(board, color)
+    puts "Pick a piece to move."
+    piece_pos = gets.chomp.split(",").map(&:strip).map { |coord| Integer(coord) }
+    raise "That isn't your piece!" if board.enemy?(piece_pos, color)
+    raise "There is no piece there!" if board.empty?(piece_pos)
+    raise "That is not even a coordinate" unless board.on_board?(piece_pos)
+    puts "Where do you want to move to?"
+    move_to_pos = gets.chomp.split(",").map(&:strip).map { |coord| Integer(coord) }
+    board.move(piece_pos, move_to_pos)
+  end
+end
+
+John = HumanPlayer.new("John")
+
+Harold = HumanPlayer.new("Harold")
+
+funtimes = Game.new({:white => John, :black => Harold})
+
+funtimes.play
